43ad05562b67544e22ed4fda3b809ab04b7609a3
Nikita Zavartsev <nikoss9644@gmail.com>
2020-04-24 11:01:09 +0600

    add function to close items

--- script.js
+++ script.js
@@ -6,10 +6,33 @@ $(function() {
       let itemName = document.querySelector('.input-name').value;
       let itemDescription = document.querySelector('.input-description').value;
       let newItem = $('<div class="item"></div>');
-      newItem.append(`<div class="item-name">${itemName}</div>`);
+      newItem.append(
+        `<div class="item-name">
+          <div>${itemName}</div>
+          <div><img class="close-item" src="/resources/img/closeMark.jpg" alt="closeMark"></div>
+        </div>`
+        );
       newItem.append(`<div class="item-description">${itemDescription}</div>`);
       newItem.appendTo($('#left-col'));
       document.querySelector('.input-name').value = '';
       document.querySelector('.input-description').value = '';
     });
+
+    $('.close-item')
+      .click(function() {
+        $(this).parents('.item').remove();
+      });
 });
+
+
+function wait() {
+  let closeItem = document.querySelectorAll('.close-item');
+  if (closeItem[0] != undefined) {
+    console.log(2);
+    for (let i = 0; i < closeItem.length; i++) {
+      closeItem[i].addEventListener('click', event => event.target.parentNode.parentNode.parentNode.remove());
+    }
+    
+  }  
+}
+document.addEventListener('click', wait);
