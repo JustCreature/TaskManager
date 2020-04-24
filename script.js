cfb9ac9f96e179ff3a9c9632c83760649884ac71
Nikita Zavartsev <nikoss9644@gmail.com>
2020-04-24 09:46:52 +0600

    add append new item to the list

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 $(function() {
   $('#add-item')
     .click(function() {
-      console.log(1);
+      $('#list-empty').remove();
       
-      let newItem = $('<div class="item"></div>')
-      newItem.innerHTML = '<div class="item-name"></div><div class="item-description"></div>'
-      newItem.appendTo($('#left-col'))
+      let itemName = document.querySelector('.input-name').value;
+      let itemDescription = document.querySelector('.input-description').value;
+      let newItem = $('<div class="item"></div>');
+      newItem.append(`<div class="item-name">${itemName}</div>`);
+      newItem.append(`<div class="item-description">${itemDescription}</div>`);
+      newItem.appendTo($('#left-col'));
+      document.querySelector('.input-name').value = '';
+      document.querySelector('.input-description').value = '';
     });
 });
